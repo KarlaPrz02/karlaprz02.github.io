7d99e4bf374967caaab337a255249af7b7c5d255
Karla <imcarla2002@gmail.com>
2025-03-02 02:18:31 +0100

    Rework again lol

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ console.clear();
 const featuresEl = document.body;
 const featureEls = document.querySelectorAll(".container-border");
 
-featuresEl.addEventListener("pointermove", (ev) => {
-  featureEls.forEach((featureEl) => {
-    const rect = featureEl.getBoundingClientRect();
-    const x = Math.max(0, ev.clientX - rect.left);
-    const y = Math.max(0, ev.clientY - rect.top);
+featuresEl.addEventListener("mousemove", (ev) => {
+  console.log(ev)
+  if (!ev.sourceCapabilities.firesTouchEvents) {
+    featureEls.forEach((featureEl) => {
+      const rect = featureEl.getBoundingClientRect();
+      const x = Math.max(0, ev.clientX - rect.left);
+      const y = Math.max(0, ev.clientY - rect.top);
 
-    featureEl.style.setProperty("--x", x);
-    featureEl.style.setProperty("--y", y);
-  });
+      featureEl.style.setProperty("--x", x);
+      featureEl.style.setProperty("--y", y);
+    });
+  }
+});
+
+featuresEl.addEventListener("mouseout", (ev) => {
+  if (!ev.sourceCapabilities.firesTouchEvents) {
+    featureEls.forEach((featureEl) => {
+      featureEl.style.setProperty("--x", 0);
+      featureEl.style.setProperty("--y", 0);
+    });
+  }
 });
